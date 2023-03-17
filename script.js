5777a6c6e143d1a70efaa5436b707e639d21b145
guru28272 <117896953+guru28272@users.noreply.github.com>
2023-03-17 12:16:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const output = document.querySelector(`#output`);
+function getData() {
+  const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(`Hello, world!`);
+    }, 1000);
+  });
+  return myPromise;
+}
+
+getData().then((res) => {
+  //   console.log(res);
+  output.innerHTML = res;
+});
